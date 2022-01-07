936abe2891375d4c0cb59ce7fd29ed6d8a13cb64
Richard Novenius <novenius@student.chalmers.se>
2022-01-07 12:02:53 +0100

    fixed schema not working issue

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -6,21 +6,15 @@ mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true})
 .catch((err) => console.log(err))
 const client =  mongoose.connection;
 client.on('error', console.error.bind(console, 'MongoDB connection error:'));
-var clinicId = "61d5f712d62e979a1e44cb57";
+
 
  
     var a = async function createClinic(newClinic){ 
         let clinic = await new Clinic(newClinic)
-        var hasArrived = false
+       // console.log(JSON.stringify(clinic))
             clinic.save(function(err, clinic){
             if(err){
                 console.log(err)
-                return hasArrived = false;
-            }else{
-                clinicId = clinic._id.toString()
-                module.exports = {clinicId}
-                console.log(clinicId)
-                return hasArrived = true;
             }
     })}
 
